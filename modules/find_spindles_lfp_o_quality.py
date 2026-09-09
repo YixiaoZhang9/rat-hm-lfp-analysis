Fix AR pole calculation in spindle quality module

modules/find_spindles_lfp_o_quality.py
@@ -33,7 +33,7 @@ def _fit_window(window, ar_order, target_fs, spindle_band):
     """Fit AR model to a single window and return (best_r, best_freq)."""
     try:
         a, _ = sm.regression.linear_model.burg(window, order=ar_order, demean=False)
-        poles = np.roots(np.r_[1, a])
+        poles = np.roots(np.r_[1, -a])
         poles = poles[np.imag(poles) > 0]
         frequencies = np.angle(poles) * target_fs / (2 * np.pi)
         r_values = np.abs(poles)